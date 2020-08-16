e6d68add55f5ec858c08599c29960d17929d3a86
Harsh Rathore <mail@harshrathore.me>
2020-08-16 16:39:19 +0530

    Conversion to json ready

--- application/models.py
+++ application/models.py
@@ -1,30 +1,16 @@
 import pandas as pd
-from flask_mysqldb import MySQL
+import json
 
 from application import app
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'myDb'
+def query(year):
+    df = pd.read_csv("dataset/dataset.csv")
+    r = df[(df['Year'] == year)]
+    return r[['name', 'id', 'Population', 'Volume in Tonnes', 'Protein Supply', 'Consumption']]
 
-mysql = MySQL(app)
-
-def ashayFun(startYear, endYear, columnRequired = '*'):
-    f = open('output.csv', 'w')
-    query='select' + columnRequired + 'from mydata where year>=' + str(startYear) + 'and year<= ' + str(endYear)
-
-    cursor.execute(query)
-
-    while True:
-        df = pd.DataFrame(cursor.fetchmany(1000))
-
-        if len(df) == 0:
-            break
-
-        else:
-            df.to_csv(f, header=False,  encoding='utf-8')
-
-    # Clean up
-    f.close()
-    cursor.close()
+def createCSV(country, startYear, endYear, columnRequired):
+    dataframe = query(startYear)
+    dict = dataframe.to_dict('records')
+    with open('dataset/output.json', 'w') as json_file:
+        json.dump(dict, json_file)
+    return 'output.json'
--- application/routes.py
+++ application/routes.py
@@ -1,12 +1,15 @@
 from flask import Flask, redirect, render_template, request, session, url_for
-import models
+from application import models, app
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/supply', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        pass
-        # CALL THE FUNCTION HERE
+        startYear = request.form.get('startYear')
+        endYear = request.form.get('endYear')
+        columnRequired = request.form.get('columnRequired')
+        country = request.form.get('country')
+        csv_file = models.createCSV(country, startYear, endYear, columnRequired)
         return render_template('supply.html')
-
+    csv_file = models.createCSV('india', 1990, 2000, 'population')
     return render_template('supply.html')
